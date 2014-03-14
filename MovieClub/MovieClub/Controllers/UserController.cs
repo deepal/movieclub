423eea3d629816c35105936e9dafd23466c691d4
Deepal Priyadarshana <dpjayasekara@live.com>
2014-03-14 19:20:33 +0530

    Started My Account page

---
+++ MovieClub/MovieClub/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieClub.Controllers
+{
+    public class UserController : Controller
+    {
+        //
+        // GET: /User/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult MyAccount()
+        {
+
+            return View();
+        }
+    }
+}

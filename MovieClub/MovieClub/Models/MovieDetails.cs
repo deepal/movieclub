b2c96d6e0f34d0f0a627f83685f569d5963af34b
Deepal Priyadarshana <dpjayasekara@live.com>
2014-03-19 13:29:12 +0530

    Admin Page Add movie started

--- MovieClub/MovieClub/Models/MovieDetails.cs
+++ MovieClub/MovieClub/Models/MovieDetails.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,19 +9,25 @@ namespace MovieClub.Models
     public class MovieDetails
     {
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
         public string Year { get; set; }
-        public string ReleaseDate { get; set; }
+        public DateTime ReleaseDate { get; set; }
         public int Runtime { get; set; }
+        [Required]
         public string Genre { get; set; }
         public string Director { get; set; }
         public string Actors { get; set; }
         public string Writer { get; set; }
+        [Required]
         public string PlotShort { get; set; }
+        [Required]
         public string PlotFull { get; set; }
         public string Language { get; set; }
         public string Country { get; set; }
         public string Awards { get; set; }
+        [Required]
         public string PosterURL { get; set; }
         public float ImdbRatings { get; set; }
         public long ImdbVotes { get; set; }

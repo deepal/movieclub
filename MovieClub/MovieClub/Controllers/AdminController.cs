fe47901856d8f00a17208bc12483715e097a8d31
Deepal Priyadarshana <dpjayasekara@live.com>
2014-03-29 11:54:05 +0530

    Added movie name suggestions in admin page

--- MovieClub/MovieClub/Controllers/AdminController.cs
+++ MovieClub/MovieClub/Controllers/AdminController.cs
@@ -50,7 +50,19 @@ namespace MovieClub.Controllers
 
             if (ModelState.IsValid)
             {
-                
+
+                MovieDB.MovieClubDBE db = new MovieDB.MovieClubDBE();
+                var movieexists = db.DBMovies.Count(m => m.ImdbId == movie.ImdbId);
+
+                if (movieexists != 0)
+                {
+
+                    return Json(new
+                    {
+                        result = "error",
+                        message = "\""+movie.Name+"\" already exists!"
+                    });
+                }
 
                 //upload trailer start
                 if (uploadFile != null)
@@ -64,8 +76,10 @@ namespace MovieClub.Controllers
                         }
                         else
                         {
-                            ModelState.AddModelError("", "Could not upload trailer! Video file extension should be \".mp4\" ");
-                            return View(movie);
+                            return Json(new {
+                                result = "error",
+                                message = "Could not upload trailer! Video file extension should be \".mp4\" "
+                            });
                         }
                     }
                 }
@@ -73,23 +87,27 @@ namespace MovieClub.Controllers
                 //if video was uploaded successfully, then save the movie poster
                 SavePoster(movie.PosterURL, movie.ImdbId);
                 //save movie to db
-                MovieDB.MovieClubDBE db = new MovieDB.MovieClubDBE();
                 if (AddMovieToDB(db, movie))
                 {
-                    ViewBag.SuccessMessage = "Movie \"" + movie.Name + "\" added successfully!";
-                    ModelState.Clear();
+                    return Json(new {
+                        result="ok",
+                        message = "Movie \"" + movie.Name + "\" added successfully!"
+                    });
                 }
                 else
                 {
-                    ModelState.AddModelError("", "Could not add movie to the database! Check your connection.");
+                    return Json(new {
+                        result = "fail",
+                        message = "Could not add movie to the database! Check your connection."
+                    });
                 }
 
 
-                return View();
-
             }
-            ModelState.AddModelError("", "Error occured adding movie! Check whether required fields are filled.");
-            return View(movie);
+            return Json(new {
+                result = "fail",
+                message = "Error occured adding movie! Check whether required fields are filled."
+            });
         }
 
 
@@ -152,6 +170,71 @@ namespace MovieClub.Controllers
             
         }
 
+        [HttpGet]
+        public ActionResult PendingReturns()
+        {
+            MovieDB.MovieClubDBE db = new MovieDB.MovieClubDBE();
+            var pendings = db.DBRents.Where(r=>(r.DueDate<DateTime.Now && r.Returned==0)||(r.Returned==0)).Join(db.DBMovies,
+                    l=>l.MovieId,
+                    r=>r.Id,
+                    (r,l)=>new{
+                        MovieId = l.Id,
+                        MovieName = l.Name,
+                        DateTaken = r.BorrowedDate,
+                        DueDate = r.DueDate,
+                        UserId = r.UserId
+                    }
+                ).Join(db.DBUsers,
+                    l=>l.UserId,
+                    r=>r.UserId,
+                    (l, r) => new
+                    {
+                        MovieId = l.MovieId,
+                        MovieName = l.MovieName,
+                        DateTaken = l.DateTaken,
+                        DueDate = l.DueDate,
+                        UserId = l.UserId,
+                        Username = r.UserName,
+                        UserEmail = r.Email
+                    }
+                );
+            List<Models.AdminModels.PendingReturnsModel> preturns = new List<Models.AdminModels.PendingReturnsModel>();
+
+            foreach (var item in pendings)
+            {
+                preturns.Add(new Models.AdminModels.PendingReturnsModel(){
+                    MovieId = item.MovieId,
+                    MovieName = item.MovieName,
+                    UserId = (int)item.UserId,
+                    UserName = item.Username,
+                    Email = item.UserEmail,
+                    DueDate = (DateTime)item.DueDate,
+                    DateTaken = (DateTime)item.DateTaken                    
+                });
+            }
+
+            return PartialView("_PendingReturnsPartial", preturns);
+        }
+
+        [HttpGet]
+        public ActionResult MarkAsReturned(int movieid, int u)
+        {
+            MovieDB.MovieClubDBE db = new MovieDB.MovieClubDBE();
+            db.DBRents.Where(r => r.UserId == u && r.MovieId == movieid).ToList().ForEach(m=>m.Returned=1);
+            return Json(new
+            {
+                result="ok"
+            });
+        }
+
+        [HttpGet]
+        public ActionResult SendMessage(int userid)
+        {
+            return Json(new
+            {
+                result = "ok"
+            });
+        } 
 
         public void SavePoster(string Url, string ImdbId)
         {
---
+++ MovieClub/MovieClub/Models/AdminModels/PendingReturnsModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieClub.Models.AdminModels
+{
+    public class PendingReturnsModel
+    {
+        public int MovieId { get; set; }
+        public string MovieName { get; set; }
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public DateTime DateTaken { get; set; }
+        public DateTime DueDate { get; set; }
+    }
+}

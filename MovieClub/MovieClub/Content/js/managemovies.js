b7e99ec691bdc0c9b5647b131895cdccad023b05
Deepal Jayasekara <dpjayasekara@live.com>
2014-05-08 18:35:33 +0530

    commit

---
+++ MovieClub/MovieClub/Content/js/managemovies.js
@@ -0,0 +1,85 @@
+﻿function deletemovie(movieid) {
+    $.ajax({
+        url: "/Admin/DeleteMovie/?movieid=" + movieid,
+        type: "POST",
+        data: AddAntiForgeryToken({}),
+        dataType: "text"
+    })
+    .success(function (data) {
+        var obj = $.parseJSON(data);
+        if (obj.result == "ok") {
+            toastr.success(obj.message)
+            $.ajax({
+                url: "/Admin/ManageMovies/?q=" + $("#search-movie-admin").val(),
+                type: "GET"
+            })
+            .success(function (data) {
+                $("#movies-list").html(data);
+            })
+            .error(function () {
+                $("#movies-list").html("Error loading movies list");
+            });
+        }
+        else if (obj.result == "error") {
+            toastr.error("Could not delete movie!")
+        }
+    })
+    .error(function () {
+        toastr.error("Could not delete movie! Check your connection.")
+    });
+}
+
+
+
+$("#search-movie-admin").keyup(function (event) {
+    $.ajax({
+        url: "/Admin/ManageMovies/?q=" + $("#search-movie-admin").val(),
+        type: "GET"
+    })
+    .success(function (data) {
+        $("#movies-list").html(data);
+    })
+    .error(function () {
+        $("#movies-list").html("Error loading movies list. Try refreshing the page.");
+    });;
+});
+
+
+function updateMoviesList() {
+    $.ajax({
+        url: "/Admin/ManageMovies",
+        type: "GET"
+    })
+    .success(function (data) {
+        $("#movies-list").html(data);
+    })
+    .error(function () {
+        $("#movies-list").html("Error loading movies list");
+    });
+}
+
+$(".featuredcheckbox").change(function (e) {
+
+    $.ajax({
+        url: "/Admin/FeatureMovie/?",
+        type: "post",
+        data: AddAntiForgeryToken({
+            movieid: $(this).attr("data-movieid"),
+            featured: $(this).is(":checked")
+        }),
+        dataType: "text"
+    })
+    .success(function (data) {
+        var obj = $.parseJSON(data);
+        if (obj.result == "ok") {
+            toastr.success(obj.message);
+        }
+        else if (obj.result == "error") {
+            $(this).removeAttr("checked");
+            toastr.warning(obj.message);
+        }
+    })
+    .error(function () {
+        toastr.error("Error occured! Check your connection.");
+    });
+});
